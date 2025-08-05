e8e394dac1beebce7aff783b2c238121c5ec9df9
ID24-CMR <idrice.tsafouet@yahoo.com>
2025-08-05 07:10:01 +0200

    update 02

--- idrice24/js/main.js
+++ idrice24/js/main.js
@@ -1,7 +1,17 @@
+
 document.addEventListener("DOMContentLoaded", function(){
     
         fetch('idrice24/footer.html').then(res => res.text()).then(data => {
             document.getElementById('footer').innerHTML = data;
+        }).catch((error) => {
+            console.error("Error loading footer:", error);
+        });
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+        fetch('idrice24/header.html').then(res => res.text()).then(data => {
+            document.getElementById('header').innerHTML = data;
         }).catch((error) => {
             console.error("Error loading footer:", error);
         });
