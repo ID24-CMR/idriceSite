1cf9bccc4ea464088f128bd26c8d9e1209cd3a9c
ID24-CMR <idrice.tsafouet@yahoo.com>
2025-08-13 08:38:18 +0200

    update files #12

--- idrice24/js/main.js
+++ idrice24/js/main.js
@@ -16,3 +16,29 @@ document.addEventListener("DOMContentLoaded", function(){
             console.error("Error loading footer:", error);
         });
 })
+
+/*function googleTranslateElementInit(){
+    new google.translate.TranslateElement(
+        { pageLanguage: 'en'},
+        'google_translate_element');
+}*/
+
+/* Script to trigger translation on dropdown change*/
+
+document.getElementById('language-select').addEventListener('change', function() {
+    var lang = this.value;
+    if(!lang) return; // do nothing if no language selected
+
+    var iframe = document.querySelector('iframe.google-te-banner-frame');
+    if(!iframe){
+        alert('Google Translate not loaded yet. please try again.');
+        return;
+    }
+
+    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var select = innerDoc.querySelector('select.goog-te-combo');
+    if(select){
+        select.value = lang;
+        select.dispatchEvent(new Event('change'));
+    }
+});
